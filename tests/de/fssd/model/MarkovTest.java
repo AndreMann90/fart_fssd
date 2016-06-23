e2a322bc05819e4648a6af752277c133154281df
Gregor Best <gbe@unobtanium.de>
2016-06-23 11:58:21 +0200

    Cleaned up, more comprehensive test for markov chains

--- tests/de/fssd/model/MarkovTest.java
+++ tests/de/fssd/model/MarkovTest.java
@@ -1,31 +1,36 @@
 package de.fssd.model;
 
-import de.fssd.dataobjects.FaultTree;
 import de.fssd.parser.Parser;
-import javafx.util.Pair;
-import jdd.bdd.BDD;
+import org.junit.Assert;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.ArrayList;
 
-/**
- * Created by gbe on 6/23/16.
- */
 public class MarkovTest {
     @org.junit.Test
     public void testMarkov() throws IOException {
         Parser p = new Parser();
 
-        FaultTree t = p.parse(new File("testcases/HFTTestCase.json"));
-
-        BDDBuilder b = new BDDBuilder();
-
-        BDDBuildResult r = b.build(t);
+        BDDBuildResult r = new BDDBuilder().build(p.parse(new File("testcases/HFTTestCase.json")));
 
         Markov m = r.m;
 
+        ArrayList<Float> vs = new ArrayList<>();
+
         for (Integer varId: r.stateMap.keySet()) {
-            System.out.println("Var: " + r.stateMap.get(varId).getId() + " : " + m.getVarState(10, varId));
+            Float s = m.getVarState(1000, varId);
+            vs.add(s);
+
+            System.out.println("Var: " + r.stateMap.get(varId).getId() + " : " + s);
+            Assert.assertTrue(s >= 0 && s <= 1);
         }
+
+        Assert.assertTrue(vs.get(0) > 0);
+        Assert.assertTrue(vs.get(1) > 0);
+        Assert.assertTrue(vs.get(2) > 0);
+        Assert.assertTrue(vs.get(3) == 0);
+        Assert.assertTrue(vs.get(4) > 0);
+        Assert.assertTrue(vs.get(5) > 0);
     }
 }
