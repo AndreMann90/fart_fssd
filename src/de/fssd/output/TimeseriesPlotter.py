9998b151b6f972d6292f811a30b64ec65b17a614
Gregor Best <gbe@unobtanium.de>
2016-07-14 10:23:00 +0200

    Add file name argument to plotter

--- src/de/fssd/output/TimeseriesPlotter.py
+++ src/de/fssd/output/TimeseriesPlotter.py
@@ -1,28 +1,42 @@
+from __future__ import print_function
 import plotly.graph_objs as go
 import plotly
 import csv
+import sys
 
-with open("data.csv") as f: # TODO get file location as parameter
-    reader = csv.reader(f)
 
-    header = next(reader)[1:]  # header without first column (time)
-    time = []
-    series = {}
-    for h in header:
-        series[h] = []
-    for row in reader:
-        time.append(row[0])
-        for i, h in enumerate(header):
-            series[h].append(row[i+1])
+def plot(fname="data.csv", output="timeseries.html"):
+    with open(fname) as f:
+        reader = csv.reader(f)
 
-    data = []
-    for key in series.keys():
-        trace = go.Scatter(
-            x=time,
-            y=series[key],
-            mode='lines',
-            name=key
-        )
-        data.append(trace)
+        header = next(reader)[1:]  # header without first column (time)
+        time = []
+        series = {}
+        for h in header:
+            series[h] = []
+        for row in reader:
+            time.append(row[0])
+            for i, h in enumerate(header):
+                series[h].append(row[i+1])
 
-    plotly.offline.plot(data, filename='timeseries.html')
+        data = []
+        for key in series.keys():
+            trace = go.Scatter(
+                x=time,
+                y=series[key],
+                mode='lines',
+                name=key
+            )
+            data.append(trace)
+
+        plotly.offline.plot(data, filename=output)
+
+if __name__ == "__main__":
+    try:
+        try:
+            plot(sys.argv[1], sys.argv[2])
+        except IndexError:
+            plot(sys.argv[1])
+    except (KeyError, IndexError):
+        sys.stderr.write("Usage: {prog} input.csv [output.html]\n".format(prog=sys.argv[0]))
+        sys.stderr.flush()
